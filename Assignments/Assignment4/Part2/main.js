b2777866f1040b9e83b3ba4f4ebc33ba11864bdd
NoorGhaniDC <noor.ghani@dcmail.ca>
2024-04-01 17:02:28 -0400

    Declared the array of image filenames.

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -5,10 +5,6 @@
 	An image gallery.
 	*/
 
-
-
-
-
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -16,6 +12,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["pic1.jpg","pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const alts = {
+    'pic1.jpg' : 'An Eye',
+    'pic2.jpg' : 'A Rock',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Hieroglyphics',
+    'pic5.jpg' : 'Moth'
+
+}
 
 /* Declaring the alternative text for each image file */
 
