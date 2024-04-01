89a8ad7cd1185d96e2a6b63ac0c63d516f64483b
NoorGhaniDC <noor.ghani@dcmail.ca>
2024-04-01 17:08:41 -0400

    Updated.

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -6,15 +6,13 @@
 	*/
 
 const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const thumbBar = ocument.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ["pic1.jpg","pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
-
-/* Declaring the alternative text for each image file */
 const alts = {
     'pic1.jpg' : 'An Eye',
     'pic2.jpg' : 'A Rock',
@@ -23,6 +21,9 @@ const alts = {
     'pic5.jpg' : 'Moth'
 
 }
+
+/* Declaring the alternative text for each image file */
+
 
 /* Looping through images */
 for (const image of images){
@@ -36,9 +37,5 @@ for (const image of images){
     });
 }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
