85b19ea9321d6273a8827cc0cc087e13fc30f268
NoorGhaniDC <noor.ghani@dcmail.ca>
2024-04-01 17:09:17 -0400

    Updated.

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -13,6 +13,8 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ["pic1.jpg","pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
 const alts = {
     'pic1.jpg' : 'An Eye',
     'pic2.jpg' : 'A Rock',
@@ -21,9 +23,6 @@ const alts = {
     'pic5.jpg' : 'Moth'
 
 }
-
-/* Declaring the alternative text for each image file */
-
 
 /* Looping through images */
 for (const image of images){
