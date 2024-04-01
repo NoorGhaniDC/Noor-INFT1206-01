f24315989fefc09cfd99052a6b4023fa0d2fadc1
NoorGhaniDC <noor.ghani@dcmail.ca>
2024-04-01 18:15:39 -0400

    Small changes.

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -23,7 +23,7 @@ for (const image of images){
     newImage.setAttribute('src', `images/${image}`);
     newImage.setAttribute('alt', alts[image]);
     thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', e =>{
+    newImage.addEventListener('click', e => {
         displayedImage.src = e.target.src;
         displayedImage.alt = e.target.alt;
     });
@@ -32,15 +32,15 @@ for (const image of images){
 
 
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener("click", () => {
-    const btnClass = btn.getAttribute("class")
-    if(btnClass === "dark"){
-        btn.setAttribute("class", "light");
-        btn.textContent = "Lighten";
-        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark'){
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     }else{
-        btn.setAttribute("class", "dark");
-        btn.textContent = "Darken";
-        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
 })
