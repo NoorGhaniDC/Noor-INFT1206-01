28eb5e90759dd9cbce384e5a08e7afc9c77234d8
NoorGhaniDC <noor.ghani@dcmail.ca>
2024-04-04 10:10:39 -0400

    Declared array, alt text, and set up loop.

--- Assignments/Assignment4/Part2/main.js
+++ Assignments/Assignment4/Part2/main.js
@@ -11,10 +11,30 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [
+    'pic1.jpg',
+    'pic2.jpg',
+    'pic3.jpg',
+    'pic4.jpg',
+    'pic5.jpg'
+]
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    'pic1.jpg': 'An eye.',
+    'pic2.jpg': 'A painting',
+    'pic3.jpg': 'A bed of flowers',
+    'pic4.jpg': 'Egyptian drawnings',
+    'pic5.jpg': 'A moth'
+}
 
 /* Looping through images */
+images.forEach((imageName) => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${imageName}`);
+    newImage.setAttribute('alt', altText[imageName]);
+    thumbBar.appendChild(newImage);
+});
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
